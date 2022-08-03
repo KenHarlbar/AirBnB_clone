44d8ae524ef668ff0e9a2b9ed471dba949beded9
Hexdee <lukmansaheed79@gmail.com>
2022-08-03 12:16:39 +0100

    Create to_dict(), console and AUTHORS file

--- models/base_model.py
+++ models/base_model.py
@@ -4,33 +4,41 @@
 from datetime import datetime
 import uuid
 
+
 class BaseModel:
-	""" class BaseModel that defines all common
-	attributes/methods for other classes """
+    """ class BaseModel that defines all common attributes/methods
+    for other classes """
 
-	def __init__(self):
-		""" Initializes the instance """
+    def __init__(self, *args, **kwargs):
+        """ Initializes the instance """
 
-		self.id = uuid.uuid4()
-		self.created_at = datetime.now()
-		self.updated_at = datetime.now()
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            self.id = kwargs["id"]
+            self.created_at = datetime.fromisoformat(kwargs["created_at"])
+            self.updated_at = datetime.fromisoformat(kwargs["updated_at"])
 
-	def __str__(self):
-		""" Returns string representation of instance """
+    def __str__(self):
+        """ Returns string representation of instance """
 
-		return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
-	def save(self):
-		""" Updates the public instance attribute updated_at """
+    def save(self):
+        """ Updates the public instance attribute updated_at """
 
-		self.updated_at = datetime.now()
+        self.updated_at = datetime.now()
 
-	def to_dict(self):
-		""" Returns a dictionary containing all keys/values
-		of __dict__ of the instance """
+    def to_dict(self):
+        """ Returns a dictionary containing all keys/values
+        of __dict__ of the instance """
 
-		return_dict = self.__dict__.copy()
-		return_dict["__class__"] = self.__class__.__name__
-		return_dict["created_at"] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-		return_dict["updated_at"] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-		return return_dict
+        return_dict = self.__dict__.copy()
+        return_dict["__class__"] = self.__class__.__name__
+        return_dict["created_at"] = self.created_at.\
+            strftime("%Y-%m-%dT%H:%M:%S.%f")
+        return_dict["updated_at"] = self.updated_at.\
+            strftime("%Y-%m-%dT%H:%M:%S.%f")
+        return return_dict
